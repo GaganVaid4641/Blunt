c019cf373490102837a0daeda8015940887b4e8b
Gagan Vaid <g4gaganvaid@gmail.com>
2018-12-23 17:03:30 +0530

    Admin css js sidebar include

---
+++ resources/views/admin/inc/admin_css.blade.php
@@ -0,0 +1,13 @@
+<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/themify-icons.css') }}">
+<link rel="stylesheet" href="{{ asset('css/metisMenu.css') }}">
+<link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
+<link rel="stylesheet" href="{{ asset('css/slicknav.min.css') }}">
+<!-- amchart css -->
+<link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
+<!-- others css -->
+<link rel="stylesheet" href="{{ asset('css/typography.css') }}">
+<link rel="stylesheet" href="{{ asset('css/default-css.css') }}">
+<link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+<link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -11,19 +11,9 @@
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" type="image/png" href="assets/images/icon/favicon.ico">
-        <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/themify-icons.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/metisMenu.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/owl.carousel.min.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/slicknav.min.css') }}">
-        <!-- amchart css -->
-        <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
-        <!-- others css -->
-        <link rel="stylesheet" href="{{ asset('css/typography.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/default-css.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+
+        @include('admin.inc.admin_css')
+
         <!-- modernizr css -->
         <script src="{{ asset('js/vendor/modernizr-2.8.3.min.js') }}"></script>    
     </head>
@@ -47,111 +37,7 @@
                         </div>
                     </div>
                     <div class="main-menu">
-                        <div class="menu-inner">
-                            <nav>
-                                <ul class="metismenu" id="menu">
-                                    <li class="active">
-                                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-dashboard"></i><span>dashboard</span></a>
-                                        <ul class="collapse">
-                                            <li><a href="index.html">ICO dashboard</a></li>
-                                            <li><a href="index2.html">Ecommerce dashboard</a></li>
-                                            <li class="active"><a href="index3.html">SEO dashboard</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layout-sidebar-left"></i><span>Sidebar
-                                                Types
-                                            </span></a>
-                                        <ul class="collapse">
-                                            <li><a href="index.html">Left Sidebar</a></li>
-                                            <li><a href="index3-horizontalmenu.html">Horizontal Sidebar</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-pie-chart"></i><span>Charts</span></a>
-                                        <ul class="collapse">
-                                            <li><a href="barchart.html">bar chart</a></li>
-                                            <li><a href="linechart.html">line Chart</a></li>
-                                            <li><a href="piechart.html">pie chart</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-palette"></i><span>UI Features</span></a>
-                                        <ul class="collapse">
-                                            <li><a href="accordion.html">Accordion</a></li>
-                                            <li><a href="alert.html">Alert</a></li>
-                                            <li><a href="badge.html">Badge</a></li>
-                                            <li><a href="button.html">Button</a></li>
-                                            <li><a href="button-group.html">Button Group</a></li>
-                                            <li><a href="cards.html">Cards</a></li>
-                                            <li><a href="dropdown.html">Dropdown</a></li>
-                                            <li><a href="list-group.html">List Group</a></li>
-                                            <li><a href="media-object.html">Media Object</a></li>
-                                            <li><a href="modal.html">Modal</a></li>
-                                            <li><a href="pagination.html">Pagination</a></li>
-                                            <li><a href="popovers.html">Popover</a></li>
-                                            <li><a href="progressbar.html">Progressbar</a></li>
-                                            <li><a href="tab.html">Tab</a></li>
-                                            <li><a href="typography.html">Typography</a></li>
-                                            <li><a href="form.html">Form</a></li>
-                                            <li><a href="grid.html">grid system</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-slice"></i><span>icons</span></a>
-                                        <ul class="collapse">
-                                            <li><a href="fontawesome.html">fontawesome icons</a></li>
-                                            <li><a href="themify.html">themify icons</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-table"></i>
-                                            <span>Tables</span></a>
-                                        <ul class="collapse">
-                                            <li><a href="table-basic.html">basic table</a></li>
-                                            <li><a href="table-layout.html">table layout</a></li>
-                                            <li><a href="datatable.html">datatable</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="maps.html"><i class="ti-map-alt"></i> <span>maps</span></a></li>
-                                    <li><a href="invoice.html"><i class="ti-receipt"></i> <span>Invoice Summary</span></a></li>
-                                    <li>
-                                        <a href="javascript:void(0)" aria-expanded="true"><i class="ti-layers-alt"></i> <span>Pages</span></a>
-                                        <ul class="collapse">
-                                            <li><a href="login.html">Login</a></li>
-                                            <li><a href="login2.html">Login 2</a></li>
-                                            <li><a href="login3.html">Login 3</a></li>
-                                            <li><a href="register.html">Register</a></li>
-                                            <li><a href="register2.html">Register 2</a></li>
-                                            <li><a href="register3.html">Register 3</a></li>
-                                            <li><a href="register4.html">Register 4</a></li>
-                                            <li><a href="screenlock.html">Lock Screen</a></li>
-                                            <li><a href="screenlock2.html">Lock Screen 2</a></li>
-                                            <li><a href="reset-pass.html">reset password</a></li>
-                                            <li><a href="pricing.html">Pricing</a></li>
-                                            <li><a href="404.html">Error 404</a></li>
-                                            <li><a href="500.html">Error 500</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0)" aria-expanded="true"><i class="fa fa-align-left"></i> <span>Multi
-                                                level menu</span></a>
-                                        <ul class="collapse">
-                                            <li><a href="#">Item level (1)</a></li>
-                                            <li><a href="#">Item level (1)</a></li>
-                                            <li><a href="#" aria-expanded="true">Item level (1)</a>
-                                                <ul class="collapse">
-                                                    <li><a href="#">Item level (2)</a></li>
-                                                    <li><a href="#">Item level (2)</a></li>
-                                                    <li><a href="#">Item level (2)</a></li>
-                                                </ul>
-                                            </li>
-                                            <li><a href="#">Item level (1)</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </nav>
-                        </div>
+                        @include('admin.inc.sidebar')
                     </div>
                 </div>
                 <!-- sidebar menu area end -->
@@ -553,38 +439,6 @@
         </div>
         <script src="{{ asset('js/app.js') }}"></script>
         <!-- jquery latest version -->
-        <script src="{{ asset('js/vendor/jquery-2.2.4.min.js') }}"></script>
-        <!-- bootstrap 4 js -->
-        <script src="{{ asset('js/popper.min.js') }}"></script>
-        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-        <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
-        <script src="{{ asset('js/metisMenu.min.js') }}"></script>
-        <script src="{{ asset('js/jquery.slimscroll.min.js') }}"></script>
-        <script src="{{ asset('js/jquery.slicknav.min.js') }}"></script>
-
-        <!-- start chart js -->
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
-        <!-- start highcharts js -->
-        <script src="https://code.highcharts.com/highcharts.js"></script>
-        <script src="https://code.highcharts.com/modules/exporting.js"></script>
-        <script src="https://code.highcharts.com/modules/export-data.js"></script>
-        <!-- start amcharts -->
-        <script src="https://www.amcharts.com/lib/3/amcharts.js"></script>
-        <script src="https://www.amcharts.com/lib/3/ammap.js"></script>
-        <script src="https://www.amcharts.com/lib/3/maps/js/worldLow.js"></script>
-        <script src="https://www.amcharts.com/lib/3/serial.js"></script>
-        <script src="https://www.amcharts.com/lib/3/plugins/export/export.min.js"></script>
-        <script src="https://www.amcharts.com/lib/3/themes/light.js"></script>
-        <!-- all line chart activation -->
-        <script src="{{ asset('js/line-chart.js') }}"></script>
-        <!-- all pie chart -->
-        <script src="{{ asset('js/pie-chart.js') }}"></script>
-        <!-- all bar chart -->
-        <script src="{{ asset('js/bar-chart.js') }}"></script>
-        <!-- all map chart -->
-        <script src="{{ asset('js/maps.js') }}"></script>
-        <!-- others plugins -->
-        <script src="{{ asset('js/plugins.js') }}"></script>
-        <script src="{{ asset('js/scripts.js') }}"></script>
+        @include('admin.inc.admin_js')
     </body>
 </html>
