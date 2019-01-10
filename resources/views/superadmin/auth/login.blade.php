a695cfb9df86f622f7f8600406dd4b1c05d39450
Gagan Vaid <g4gaganvaid@gmail.com>
2019-01-10 23:36:58 +0530

    Login page design and superadmin sidebar changes

--- resources/views/superadmin/auth/login.blade.php
+++ resources/views/superadmin/auth/login.blade.php
@@ -1,69 +1,82 @@
-@extends('superadmin.layouts.admin_login_app')
+<!doctype html>
+<html class="no-js" lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Login - srtdash</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('superadmin.login') }}">
+    @include('superadmin.inc.admin_css')
+
+    <!-- modernizr css -->
+    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+
+<body>
+    <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+    <!-- preloader area start -->
+    <div id="preloader">
+        <div class="loader"></div>
+    </div>
+    <!-- preloader area end -->
+    <!-- login area start -->
+    <div class="login-area login-bg">
+        <div class="container">
+            <div class="login-box ptb--100">
+                <form method="POST" action="{{ route('superadmin.login') }}">
                         {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                    <div class="login-form-head">
+                        <h4>Sign In</h4>
+                        <p>Hello there, Sign in and start managing your Admin Template</p>
+                    </div>
+                    <div class="login-form-body">
+                        <div class="form-gp">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input id="exampleInputEmail1" type="email" name="email" value="{{ old('email') }}" required autofocus>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('email') }}</strong>
+                                </span>
+                            @endif
+                            <i class="ti-email"></i>
                         </div>
-
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
+                        <div class="form-gp">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input id="exampleInputPassword1" type="password" name="password" required>
+                            @if ($errors->has('password'))
+                                <span class="help-block">
+                                    <strong>{{ $errors->first('password') }}</strong>
+                                </span>
+                            @endif
+                            <i class="ti-lock"></i>
                         </div>
+                        <div class="row mb-4 rmber-area">
+                            <div class="col-6">
+                                <div class="custom-control custom-checkbox mr-sm-2">
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
+                                    <input type="checkbox" class="custom-control-input" id="customControlAutosizing" name="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <label class="custom-control-label" for="customControlAutosizing">Remember Me</label>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
+                            <div class="col-6 text-right">
+                                <a href="{{ route('password.request') }}">
+                                    Forgot Password
                                 </a>
                             </div>
                         </div>
-                    </form>
-                </div>
+                        <div class="submit-btn-area">
+                            <button id="form_submit" type="submit">Submit <i class="ti-arrow-right"></i></button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    <!-- login area end -->
+
+    @include('superadmin.inc.admin_js')
+</body>
+</html>
--- resources/views/superadmin/layouts/app.blade.php
+++ resources/views/superadmin/layouts/app.blade.php
@@ -37,7 +37,7 @@
                         </div>
                     </div>
                     <div class="main-menu">
-                        @include('admin.inc.sidebar')
+                        @include('superadmin.inc.sidebar')
                     </div>
                 </div>
                 <!-- sidebar menu area end -->
@@ -215,7 +215,7 @@
                         <div class="row align-items-center">
                             <div class="col-sm-6">
                                 <div class="breadcrumbs-area clearfix">
-                                    <h4 class="page-title pull-left">Dashboard</h4>
+                                    <h4 class="page-title pull-left">Super Admin Dashboard</h4>
                                     <ul class="breadcrumbs pull-left">
                                         <li><a href="index.html">Home</a></li>
                                         <li><span>Dashboard</span></li>
